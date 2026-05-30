4e742a496ceb00ce4f22a52dd93280f2157f6639
sky xu <sky.xu.me@gmail.com>
2026-05-30 14:05:51 -0700

    remove unused code

--- Pika/Pika/PikaApp.swift
+++ Pika/Pika/PikaApp.swift
@@ -17,22 +17,7 @@ struct PikaApp: App {
         let session = AVAudioSession.sharedInstance()
         try? session.setCategory(.ambient, mode: .default, options: [.mixWithOthers])
         try? session.setActive(true)
-
-//        Self.prewarmFonts()
     }
-
-//    private static func prewarmFonts() {
-//        let names = [
-//            "Telka-ExtendedBlack",
-//            "Telka-ExtendedBold",
-//            "Telka-ExtendedMedium",
-//            "Telka-Regular",
-//            "Telka-Medium",
-//        ]
-//        for name in names {
-//            _ = UIFont(name: name, size: 17)
-//        }
-//    }
 
     var body: some Scene {
         WindowGroup {
